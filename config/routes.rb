c36de761bd900274d66975838066eb98c7e71630
FrozenIce0617 <prosdev087@gmail.com>
2019-05-30 06:25:11 +1000

    Follow Devise instruction and modify navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
