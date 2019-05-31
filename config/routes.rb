77dc3f200e923dba5f82a14e53cbf8007daaa193
FrozenIce0617 <prosdev087@gmail.com>
2019-05-31 22:49:09 +1000

    Add Jumbo at unsigned user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pins
   devise_for :users
-  root 'home#index'
+  root 'pins#index'
   get 'home/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
